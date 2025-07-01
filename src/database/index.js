efbc51cc885110c355bb4edb108f7dc831414d93
Beau <150202281+beaunus-round@users.noreply.github.com>
2025-06-30 23:07:21 -0500

    idk

--- src/database/index.js
+++ src/database/index.js
@@ -2,8 +2,8 @@ module.exports = {
   client: require('./client'),
   models: {
     ClientConfig: require('./models/ClientConfig'),
-    LeadEnrichment: require('./models/LeadEnrichment'),
-    JobLog: require('./models/JobLog'),
-    FileUpload: require('./models/FileUpload')
+    LeadEnrichment: require('./LeadEnrichment'),        
+    JobLog: require('./JobLog'),                         
+    FileUpload: require('./FileUpload')                 
   }
 };
